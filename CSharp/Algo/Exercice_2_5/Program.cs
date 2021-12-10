82228790d59a6e880b431e6d678165e1ad6f98b8
Idrine <36388417+oribault@users.noreply.github.com>
2021-12-10 11:46:35 +0100

    nouvel exo

---
+++ CSharp/Algo/Exercice_2_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercice_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombreDeLutilisateur;
+            int nombreDeLordinateur;
+
+            Console.WriteLine("Choisir un nombre entre 0 1 et 3");
+            nombreDeLutilisateur = int.Parse(Console.ReadLine());
+
+            Random rand = new Random();
+
+            nombreDeLordinateur = rand.Next(0, 3);
+        }
+    }
+}
