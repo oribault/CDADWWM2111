bc5ec21783a3e2601ae16e7d23061732f74367a0
Idrine <36388417+oribault@users.noreply.github.com>
2021-12-15 10:34:29 +0100

    Nouveau dossier

---
+++ CSharp/Algo/Exercice_1_1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercice_1_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
