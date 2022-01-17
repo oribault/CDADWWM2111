303f8ffaf70065dbc65b96fe2ae5212bc207d752
Idrine <36388417+oribault@users.noreply.github.com>
2022-01-17 14:19:43 +0100

    import

---
+++ Trousse/Trousse/Trousse/Trousse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trousse
+{
+    public abstract class Trousse
+    {
+
+        private int _volume;
+        private bool _estOuverte;
+        public Trousse(int volume, bool estOuverte)
+        {
+            Volume = volume;
+            EstOuverte = estOuverte;
+        }
+        public int Volume
+        {
+            get { return _volume; }
+            private set { _volume = value; }
+        }
+        public bool EstOuverte
+        {
+            get { return _estOuverte; }
+            private set { _estOuverte = value; }
+        }
+
+        public Trousse()
+        {
+            Volume = 0;
+            EstOuverte = false;
+        }
+
+    }
+}
