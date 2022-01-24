ad520874ad680d660281a9990e32e1108fd8ac53
Idrine <36388417+oribault@users.noreply.github.com>
2022-01-24 16:22:16 +0100

    Nouveau dossier

---
+++ UML/Sac/Sac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ UML/Sac/Sac/Sac.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sac
+{
+    class Sac
+    {
+        //Attribut 
+        private int _volume;
+        private string _couleur;
+
+        //Methodes
+
+        //Constructor
+        public Sac(int volume, string couleur)
+        {
+            this._volume = volume;
+            this._couleur = couleur;
+        }
+        public int Volume
+        {
+            get { return _volume; }
+            private set { _volume = value; }
+        }
+        
+        public string Couleur
+        {
+            get { return _couleur; }
+            private set { _couleur = value; }
+        }
+        public void Ouvrir()//methode
+        {
+
+        }
+        public void Fermer()//methode
+        {
+
+        }
+    }
+}
