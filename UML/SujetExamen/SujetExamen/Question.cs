6cdba5fa497486a9755b9a9ab4683eff0195a055
Océane <36388417+oribault@users.noreply.github.com>
2022-02-01 07:54:21 +0100

    Add files via upload

---
+++ UML/SujetExamen/SujetExamen/Question.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SujetExamen
+{
+    class Question
+    {
+        private string _enonce;
+        private int _difficulte;
+
+        public Question(string e, int d)
+        {
+            this._enonce = e;
+            this.Difficulte = d;
+        }
+        public string Enonce
+        {
+            get { return _enonce; } init { _enonce = value; }
+        }
+        public int Difficulte
+        {
+            get { return _difficulte; }
+            set { if (value < 0) 
+                {
+                    _difficulte = 0;
+                }
+               else  if (value > 100)
+                {
+                    _difficulte = 100;
+                }
+                else
+                {
+                    _difficulte = value;
+                }
+            }
+              
+        }
+    }
+}
